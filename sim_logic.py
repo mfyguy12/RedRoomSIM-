4ecd72a79b4942b301c7dfe83661fc6de30919f9
mfyguy12 <mattfyrer@gmail.com>
2025-06-22 13:37:10 -0400

    Create sim_logic.py

---
+++ sim_logic.py
@@ -0,0 +1,9 @@
+import json
+from pathlib import Path
+from app.models.sim_models import SimScenario
+
+def load_scenario_from_file(file_name: str) -> SimScenario:
+    file_path = Path(__file__).resolve().parent.parent / "data" / file_name
+    with open(file_path) as f:
+        data = json.load(f)
+    return SimScenario(**data)
